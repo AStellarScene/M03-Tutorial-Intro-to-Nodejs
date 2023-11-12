ae240ef67a730da77af681f495d56f80a811bae7
AStellarScene <144614242+AStellarScene@users.noreply.github.com>
2023-11-12 23:26:42 +0000

    Tutorial Content & Testing

---
+++ global.js
@@ -0,0 +1,15 @@
+const speak = () => {
+  console.log('hello, ninjas');
+}
+
+setTimeout(() => {
+  console.log('Time Out');
+  clearInterval(int);
+}, 3000);
+
+const int = setInterval(() => {
+  console.log('Interval');
+}, 1000);
+
+console.log(__dirname);
+console.log(__filename);
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['Adam', 'Sam', 'Ryan', 'Jack'];
+const ages = [20, 17, 23, 14];
+
+module.exports = {
+  people,
+  ages,
+}
